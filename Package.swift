04f15faca5aaa09f5b78fa9a0e62db251efae5e7
Sergei Shirokov <sshirokov@malwarebytes.com>
2024-01-24 23:55:29 +0200

    initial package

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+
+    name: "xcode15crash",
+    targets: [
+
+        .executableTarget(
+
+            name: "xcode15crash",
+            path: "Sources"
+        ),
+    ]
+)
